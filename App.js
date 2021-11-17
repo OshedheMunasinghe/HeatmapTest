d3e1ce10e65a4a605cb4decf1c4ca886278c6864
Osh <gusmunos@student.gu.se>
2021-11-17 11:01:13 +0100

    installed react native elements. All good!

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import {StyleSheet, Text, View} from 'react-native'
 import MapScreen from './screens/MapScreen'
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<MapScreen />
-		</View>
-	)
+    return (
+        <View style={styles.container}>
+            <MapScreen/>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 })
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,79 +1,79 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import {
-	StyleSheet,
-	Text,
-	View,
-	Dimensions,
-	TouchableOpacity,
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    TouchableOpacity,
 } from 'react-native'
 import MapView from 'react-native-maps'
 import NetInfoDisplay from '../components/NetInfoDisplay'
 
 const MapScreen = () => {
-	const [mapType, setMapType] = useState('standard')
+    const [mapType, setMapType] = useState('standard')
 
-	const changeMapType = () => {
-		if (mapType === 'standard') {
-			setMapType('satellite')
-		} else if (mapType === 'satellite') {
-			setMapType('standard')
-		}
-	}
+    const changeMapType = () => {
+        if (mapType === 'standard') {
+            setMapType('satellite')
+        } else if (mapType === 'satellite') {
+            setMapType('standard')
+        }
+    }
 
-	return (
-		<View style={styles.container}>
-			<NetInfoDisplay />
-			<MapView
-				style={styles.map}
-				showsUserLocation={true}
-				showsMyLocationButton={true}
-				// followsUserLocation={true}
-				mapType={mapType}
-			/>
-			<View style={styles.chipView}>
-				<TouchableOpacity
-					style={styles.chipsItem}
-					onPress={() => changeMapType()}
-				>
-					<Text>{mapType === 'standard' ? 'Satellit' : 'Karta'}</Text>
-				</TouchableOpacity>
-			</View>
-		</View>
-	)
+    return (
+        <View style={styles.container}>
+            <NetInfoDisplay/>
+            <MapView
+                style={styles.map}
+                showsUserLocation={true}
+                showsMyLocationButton={true}
+                // followsUserLocation={true}
+                mapType={mapType}
+            />
+            <View style={styles.chipView}>
+                <TouchableOpacity
+                    style={styles.chipsItem}
+                    onPress={() => changeMapType()}
+                >
+                    <Text>{mapType === 'standard' ? 'Satellit' : 'Karta'}</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
 export default MapScreen
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	map: {
-		flex: 1,
-		width: Dimensions.get('window').width,
-		height: Dimensions.get('window').height,
-	},
-	chipsItem: {
-		flexDirection: 'row',
-		backgroundColor: '#fff',
-		borderRadius: 20,
-		padding: 8,
-		paddingHorizontal: 20,
-		marginHorizontal: 10,
-		height: 35,
-		shadowColor: '#ccc',
-		shadowOffset: { width: 0, height: 3 },
-		shadowOpacity: 0.5,
-		shadowRadius: 5,
-		elevation: 10,
-	},
-	chipView: {
-		flexDirection: 'row',
-		position: 'absolute',
-		top: Platform.OS === 'ios' ? 60 : 50,
-		paddingHorizontal: 10,
-	},
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        flex: 1,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+    chipsItem: {
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        padding: 8,
+        paddingHorizontal: 20,
+        marginHorizontal: 10,
+        height: 35,
+        shadowColor: '#ccc',
+        shadowOffset: {width: 0, height: 3},
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        elevation: 10,
+    },
+    chipView: {
+        flexDirection: 'row',
+        position: 'absolute',
+        top: Platform.OS === 'ios' ? 60 : 50,
+        paddingHorizontal: 10,
+    },
 })
