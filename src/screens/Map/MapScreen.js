7a7d4a19bdd2ad3f64bf33460fc1464075bdf78d
Osh <gusmunos@student.gu.se>
2021-11-18 17:56:32 +0100

    Fixed bugg. Now it's working.

--- src/screens/Map/MapScreen.js
+++ src/screens/Map/MapScreen.js
@@ -1,168 +1,184 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import {
-	StyleSheet,
-	Text,
-	View,
-	Dimensions,
-	TouchableOpacity,
-	ActivityIndicator,
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    TouchableOpacity,
+    ActivityIndicator, Platform, ToastAndroid
 } from 'react-native'
 import * as Location from 'expo-location'
-import MapView, { Heatmap } from 'react-native-maps'
-import NetInfoDisplay from '../components/NetInfoDisplay'
+import MapView, {Heatmap} from 'react-native-maps'
+// import NetInfoDisplay from '../components/NetInfoDisplay'
 import {Button} from "react-native-elements";
 
+const mapStyle = require('../../styles/MapStyle/MapStyle.json')
 const MapScreen = () => {
-	const [mapType, setMapType] = useState('standard')
-	const [location, setLocation] = useState({ latitude: null, longitude: null })
-	const [errorMsg, setErrorMsg] = useState(null)
-	const [isLoading, setIsLoading] = useState(true)
-	const [points, setPoints] = useState([{ latitude: 1, longitude: 1, weight: 1 }])
-	const [rec, setRec] = useState(false)
-	const [asd, setAsd] = useState()
+    const [mapType, setMapType] = useState('standard')
+    const [location, setLocation] = useState({latitude: null, longitude: null})
+    const [errorMsg, setErrorMsg] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const [points, setPoints] = useState([{latitude: 1, longitude: 1, weight: 1}])
+    const [rec, setRec] = useState(false)
+    const [asd, setAsd] = useState()
 
-	useEffect(() => {
-		(async () => {
-			setIsLoading(true)
-			let { status } = await Location.requestForegroundPermissionsAsync()
-			if (status !== 'granted') {
-				return
-			}
-			let location = await Location.getCurrentPositionAsync({})
-			setLocation({ latitude: location.coords.latitude, longitude: location.coords.longitude })
-			console.log(location)
-			setIsLoading(false)
-		})()
-	}, [])
+    useEffect(() => {
+        (async () => {
+            setIsLoading(true)
+            let {status} = await Location.requestForegroundPermissionsAsync()
+            if (status !== 'granted') {
+                return
+            }
+            let location = await Location.getCurrentPositionAsync({})
+            setLocation({latitude: location.coords.latitude, longitude: location.coords.longitude})
+            console.log(location)
+            setIsLoading(false)
+        })()
+    }, [])
 
 
-	const changeMapType = () => {
-		if (mapType === 'standard') {
-			setMapType('satellite')
-		} else if (mapType === 'satellite') {
-			setMapType('standard')
-		}
-	}
+    const changeMapType = () => {
+        if (mapType === 'standard') {
+            setMapType('satellite')
+        } else if (mapType === 'satellite') {
+            setMapType('standard')
+        }
+    }
 
-	const recording = async () => {
-		setRec(true)
+    const recording = async () => {
+        if (Platform.OS === 'android') {
+            ToastAndroid.show('Inspelningen har startat', ToastAndroid.SHORT);
+        }
+        setRec(true)
 
-		const client = await Location.watchPositionAsync({
-			accuracy: Location.Accuracy.Highest,
-			distanceInterval: 1,
-			timeInterval: 1000,
-		}, (loc) => {
-			let newValue = { latitude: loc.coords.latitude, longitude: loc.coords.longitude, weight: 1 }
-			setPoints(oldArray => [...oldArray, newValue])
-			console.log(loc.coords.latitude, loc.coords.longitude)
-		})
-		return setAsd(client)
-	}
-	const stop = async () => {
-		setRec(false)
-		console.log('Stoppad inspelning')
-		console.log(points)
-		await asd.remove()
-	}
+        const client = await Location.watchPositionAsync({
+            accuracy: Location.Accuracy.Highest,
+            distanceInterval: 1,
+            timeInterval: 1000,
+        }, (loc) => {
+            let newValue = {latitude: loc.coords.latitude, longitude: loc.coords.longitude, weight: 1}
+            setPoints(oldArray => [...oldArray, newValue])
+            console.log(loc.coords.latitude, loc.coords.longitude)
+        })
+        return setAsd(client)
+    }
+    const stop = async () => {
+        if (Platform.OS === 'android') {
+            ToastAndroid.show('Inspelningen har stoppat', ToastAndroid.SHORT);
+        }
+        setRec(false)
+        console.log('Stoppad inspelning')
+        console.log(points)
+        await asd.remove()
+    }
 
-	return (
-		<View style={styles.container}>
-			{/*<NetInfoDisplay />*/}
-			{isLoading ? (<ActivityIndicator style={styles.map} size='large' />) : (
-				<MapView
-					style={styles.map}
-					showsUserLocation={true}
-					showsMyLocationButton={true}
-					provider='google'
-					initialRegion={{
-						latitude: location.latitude,
-						longitude: location.longitude,
-						latitudeDelta: 0.0922,
-						longitudeDelta: 0.0421
-					}}
-					mapType={mapType}
-					map
-				>
-					<Heatmap
-						initialRegion={{
-							latitude: location.latitude,
-							longitude: location.longitude,
-							latitudeDelta: 0.0922,
-							longitudeDelta: 0.0421
-						}}
-						points={points}
-						radius={50}
-						gradient={{
-							colors: ['darkgreen', 'lightgreen', 'yellow', 'orange', 'red'],
-							startPoints: [0.01, 0.04, 0.1, 0.45, 0.5],
-							colorMapSize: 200
-						}}
-					/>
-				</MapView>)}
+    return (
+        <View style={styles.container}>
+            {/*<NetInfoDisplay />*/}
+            {isLoading ? (<ActivityIndicator style={styles.map} size='large'/>) : (
+                <MapView
+                    style={styles.map}
+                    showsUserLocation={true}
+                    showsMyLocationButton={true}
+                    provider='google'
+                    initialRegion={{
+                        latitude: location.latitude,
+                        longitude: location.longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421
+                    }}
+                    mapType={mapType}
+                    customMapStyle={mapStyle}
+                >
+                    <Heatmap
+                        initialRegion={{
+                            latitude: location.latitude,
+                            longitude: location.longitude,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421
+                        }}
+                        points={points}
+                        radius={50}
+                        gradient={{
+                            colors: ['darkgreen', 'lightgreen', 'yellow', 'orange', 'red'],
+                            startPoints: [0.01, 0.04, 0.1, 0.45, 0.5],
+                            colorMapSize: 200
+                        }}
+                    />
+                </MapView>)}
 
-			<View style={styles.buttonContainer}>
-				<Button title={'■'} buttonStyle={{backgroundColor: "#000000", borderRadius: 16, width: 62, height: 62, opacity: .5}} titleStyle={{color: "red", fontSize: 20,}}/>
-			</View>
+            <View style={styles.buttonContainer}>
+                {!rec ?
+                    <Button title={'■'}
+                            buttonStyle={{backgroundColor: "#D3D3D3", borderRadius: 16, width: 62, height: 62,}}
+                            titleStyle={{color: "red", fontSize: 23,}} onPress={() => recording()}/> :
+                    <Button title={'●'}
+                            buttonStyle={{backgroundColor: "#D3D3D3", borderRadius: 16, width: 62, height: 62,}}
+                            titleStyle={{color: "black", fontSize: 23,}} onPress={() => stop()}/>
+                }
 
-			<View style={styles.chipView}>
-				<TouchableOpacity
-					style={styles.chipsItem}
-					onPress={() => changeMapType()}
-				>
-					<Text>{mapType === 'standard' ? 'Satellit' : 'Karta'}</Text>
-				</TouchableOpacity>
-				{/*{!rec ? (<TouchableOpacity style={styles.chipsItem} onPress={() => recording()}>
+
+            </View>
+
+            <View style={styles.chipView}>
+                <TouchableOpacity
+                    style={styles.chipsItem}
+                    onPress={() => changeMapType()}
+                >
+                    <Text>{mapType === 'standard' ? 'Satellit' : 'Karta'}</Text>
+                </TouchableOpacity>
+                {/*{!rec ? (<TouchableOpacity style={styles.chipsItem} onPress={() => recording()}>
 					<Text>Starta inspelningen</Text>
 				</TouchableOpacity>) : (<TouchableOpacity style={styles.chipsItem} onPress={() => stop()}>
 					<Text>Stoppa</Text>
 				</TouchableOpacity>)}*/}
-				<TouchableOpacity style={styles.chipsItem} onPress={() => setPoints([])}>
-					<Text>Radera</Text>
-				</TouchableOpacity>
-			</View>
-		</View>
+                <TouchableOpacity style={styles.chipsItem} onPress={() => setPoints([])}>
+                    <Text>Radera</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
 
-	)
+    )
 }
 
 export default MapScreen
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	map: {
-		flex: 1,
-		width: Dimensions.get('window').width,
-		height: Dimensions.get('window').height,
-	},
-	chipsItem: {
-		flexDirection: 'row',
-		backgroundColor: '#fff',
-		borderRadius: 20,
-		padding: 8,
-		paddingHorizontal: 20,
-		marginHorizontal: 10,
-		height: 35,
-		shadowColor: '#ccc',
-		shadowOffset: { width: 0, height: 3 },
-		shadowOpacity: 0.5,
-		shadowRadius: 5,
-		elevation: 10,
-	},
-	chipView: {
-		flexDirection: 'row',
-		position: 'absolute',
-		top: Platform.OS === 'ios' ? 60 : 50,
-		paddingHorizontal: 10,
-	},
-	buttonContainer:{
-		flexDirection: 'row',
-		position: 'absolute',
-		bottom: 38,
-		paddingHorizontal: 10,
-	}
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        flex: 1,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+    chipsItem: {
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        padding: 8,
+        paddingHorizontal: 20,
+        marginHorizontal: 10,
+        height: 35,
+        shadowColor: '#ccc',
+        shadowOffset: {width: 0, height: 3},
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        elevation: 10,
+    },
+    chipView: {
+        flexDirection: 'row',
+        position: 'absolute',
+        top: Platform.OS === 'ios' ? 60 : 50,
+        paddingHorizontal: 10,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        position: 'absolute',
+        bottom: 38,
+        paddingHorizontal: 10,
+    }
 })
