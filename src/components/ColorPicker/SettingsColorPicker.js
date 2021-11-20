97d6c2824728ba97f3e8b507105cefdd66348b19
Osh <gusmunos@student.gu.se>
2021-11-20 22:39:26 +0100

    All good before we install rgb-hsv

--- src/components/ColorPicker/SettingsColorPicker.js
+++ src/components/ColorPicker/SettingsColorPicker.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import {View} from "react-native";
-import {Button, Overlay, Text, Card, Chip} from 'react-native-elements';
-// import ColorPicker from 'react-native-wheel-color-picker'
+import {Button, Card} from 'react-native-elements';
+import ColorPicker from 'react-native-wheel-color-picker'
 
-const colors = ['darkgreen', 'lightgreen', 'yellow', 'orange', 'red']
+const colors = ['#006400', '#90ee90', '#ffff00', '#ffa500', '#ff0000']
 const SettingsColorPicker = ({props}) => {
     const [visible, setVisible] = useState(props);
+    const [picker, setPicker] = useState(null)
+    const [pickedColor, setPickedColor] = useState('')
 
     const toggleOverlay = () => {
         console.log('toggle overlay')
@@ -24,10 +26,22 @@ const SettingsColorPicker = ({props}) => {
             <Card containerStyle={{minHeight: '90%', borderRadius: 8}}>
                 <Card.Title>CARD WITH DIVIDER</Card.Title>
                 <Card.Divider/>
-                <View style={{flexDirection: "row", justifyContent: "space-evenly" }}>
+
+                {/* * color picker*/}
+
+                <View style={{ minHeight: "70%", bottom: 32}}>
+                    { pickedColor !== '' && <ColorPicker ref={ r => setPicker(r)} color={pickedColor} onColorChangeComplete={() => console.log(picker.color)}
+                    />}
+                </View>
+
+                {/* * Buttons */}
+                <View style={{flexDirection: "row", justifyContent: "space-evenly"}}>
                     {colors.map((col, id) => (
                         <View>
-                            <Button buttonStyle={{backgroundColor: col, width: 60}} title={'abc'} key={id}>Item {id}</Button>
+                            <Button buttonStyle={{backgroundColor: col, width: 60}}
+                                    title={'abc'}
+                                    key={id}
+                            onPress={ () => setPickedColor(col)}>Item {id}</Button>
                         </View>
                     ))}
                 </View>
