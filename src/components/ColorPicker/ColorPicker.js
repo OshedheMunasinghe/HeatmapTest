b8f0bdc79db31b0b97df473829d943593750adff
Ken Bäcklund <ken.backlund@iths.se>
2021-11-23 12:37:07 +0100

    Color picker now accessible directly from speed dial, bypassing setup screen

---
+++ src/components/ColorPicker/ColorPicker.js
@@ -0,0 +1,151 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, Card, Overlay } from "react-native-elements";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const ColorPicker = ({ visible, setVisible }) => {
+  const [saveButtonDisabled, setSaveButtonDisabled] = useState(true);
+  const [pickedButton, setPickedButton] = useState(-1);
+  const [buttonColors, setButtonColors] = useState([  // Default colors
+    "#0E0",
+    "#0D0",
+    "yellow",
+    "orange",
+    "red",
+  ]);
+
+  useEffect(() => {
+    return () => {
+        loadHeatmapColors()
+        console.log('useEffect: loadHeatmapColors')
+    };
+}, []);
+
+const loadHeatmapColors = async () => {
+    try {
+        let heatmapColors = buttonColors
+        const json = await AsyncStorage.getItem('@heatmap_colors')
+        if (json != null) {
+            heatmapColors = JSON.parse(json)
+            setButtonColors(heatmapColors)
+            console.log('Loaded colors from Async Storage')
+        }
+        
+    }
+    catch (e) {
+        console.log ('Error restoring data from async storage')
+    }
+}
+
+const saveHeatmapColors = async () => {
+    try {
+        await AsyncStorage.setItem('@heatmap_colors', JSON.stringify(buttonColors))
+    }
+    catch (e) {
+        console.log ('Error saving data to async storage')
+    }
+}
+
+const generateHueButtons = () => {
+    const list = [];
+    const buttons = 48;
+    for (let i = 0; i < buttons; i++) {
+      const hsl = `hsl(${Math.round((i * 360.0) / buttons)},100%,50%)`;
+      list.push(
+        <View key={i} style={{ flex: 1, padding: 0, backgroundColor: hsl }}>
+          <Button
+            onPress={() => {
+              if (pickedButton !== -1) {
+                const a = buttonColors.slice(0, pickedButton);
+                const b = buttonColors.slice(pickedButton + 1);
+                const c = a.concat(hsl).concat(b);
+                setButtonColors(c);
+                setSaveButtonDisabled(false);
+              }
+            }}
+            buttonStyle={{
+              padding: 0,
+              height: 32,
+              border: 0,
+              borderColor: "#000",
+              backgroundColor: hsl,
+            }}
+          />
+        </View>
+      );
+    }
+    return list;
+  };
+
+  const generatePaletteButtons = () => {
+    const list = [];
+    const buttons = 5;
+    for (let i = 0; i < buttons; i++) {
+      list.push(
+        <View key={i}>
+          <Button
+            buttonStyle={{
+              width: 60,
+              height: 60,
+              backgroundColor: buttonColors[i],
+            }}
+            onPress={() => {
+              setPickedButton(i);
+            }}
+          />
+        </View>
+      );
+    }
+    return list;
+  };
+
+  const saveColors = () => {
+    saveHeatmapColors();
+    setSaveButtonDisabled(false);
+    setVisible(false);
+  };
+
+  return (
+    <Overlay visible={visible} overlayStyle={{ width: "90%" }}>
+      <Card
+        containerStyle={{
+          minHeight: "40%",
+          borderRadius: 8,
+          marginBottom: 20,
+        }}
+      >
+        <Card.Title>CHOOSE COLOR</Card.Title>
+        {/* * color picker*/}
+        <View style={{ top: 28, minHeight: 64 }}>
+          <View style={{ minHeight: "8%", bottom: 32, flexDirection: "row" }}>
+            {generateHueButtons()}
+          </View>
+        </View>
+
+        <View style={{ flexDirection: "row", justifyContent: "space-evenly" }}>
+          {buttonColors && generatePaletteButtons()}
+        </View>
+        <View
+          style={{ flexDirection: "row", justifyContent: "center", top: 18 }}
+        >
+          <Button
+            title={"SPARA"}
+            buttonStyle={{ margin: 8, padding: 16 }}
+            disabled={saveButtonDisabled}
+            onPress={saveColors}
+          />
+          <Button
+            title={"AVBRYT"}
+            buttonStyle={{ margin: 8, padding: 16 }}
+            onPress={() => setVisible(false)}
+          />
+        </View>
+
+      </Card>
+    </Overlay>
+  );
+};
+
+export default ColorPicker;
+
+const styles = StyleSheet.create({});
--- src/components/SpeedOptions/SpeedOptions.js
+++ src/components/SpeedOptions/SpeedOptions.js
@@ -1,76 +1,88 @@
-import React, { useState } from 'react'
-import { StyleSheet } from 'react-native'
-import { SpeedDial } from 'react-native-elements'
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { SpeedDial, Button, Card, Overlay } from "react-native-elements";
+import ColorPicker from "../ColorPicker/ColorPicker";
 
 const SpeedOptions = (props) => {
-	const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [colorPickerVisible, setColorPickerVisible] = useState(false);
 
-	const changeMapType = (props) => {
-		if (props.mapTypeProps.mapType === 'standard') {
-			props.mapTypeProps.setMapType('satellite')
-		} else if (props.mapTypeProps.mapType === 'satellite') {
-			props.mapTypeProps.setMapType('standard')
-		}
-	}
+  const changeMapType = (props) => {
+    if (props.mapTypeProps.mapType === "standard") {
+      props.mapTypeProps.setMapType("satellite");
+    } else if (props.mapTypeProps.mapType === "satellite") {
+      props.mapTypeProps.setMapType("standard");
+    }
+  };
 
-	const showCard = (props) => {
-		if (props.cardProps.cardVisible === false) {
-			props.cardProps.setCardVisible(true)
-		} else if (props.cardProps.cardVisible === true) {
-			props.cardProps.setCardVisible(false)
-		}
-	}
+  const showCard = (props) => {
+    if (props.cardProps.cardVisible === false) {
+      props.cardProps.setCardVisible(true);
+    } else if (props.cardProps.cardVisible === true) {
+      props.cardProps.setCardVisible(false);
+    }
+  };
 
-	return (
-		<SpeedDial
-			color="#D3D3D3"
-			style={styles.speedContainer}
-			isOpen={open}
-			icon={{ name: 'edit', color: 'black' }}
-			openIcon={{ name: 'close', color: 'black' }}
-			onOpen={() => setOpen(!open)}
-			onClose={() => setOpen(!open)}
-		>
-			<SpeedDial.Action
-				titleStyle={{ fontFamily: "GenosR" }}
-				color="#D3D3D3"
-				icon={{ name: 'map', color: 'black' }}
-				title="Dashboard"
-				onPress={() => showCard(props)}
-			/>
-			<SpeedDial.Action
-				color="#D3D3D3"
-				icon={{ name: 'dashboard', color: 'black' }}
-				title={props.mapTypeProps.mapType === 'standard' ? 'Satellit' : 'Karta'}
-				onPress={() => changeMapType(props)}
-			/>
-			<SpeedDial.Action
-				color="#D3D3D3"
-				icon={{ name: 'settings', color: 'black' }}
-				title="Heatmap Colors"
-				// onPress={() => alert('-- Color Picker --')}
-				onPress={() => { props.nav.navigate('Settings') }}
-			/>
-		</SpeedDial>
-	)
-}
+  return (
+    <>
+      <View>
+        <ColorPicker
+          visible={colorPickerVisible}
+          setVisible={setColorPickerVisible}
+        />
+      </View>
+      <SpeedDial
+        color="#D3D3D3"
+        style={styles.speedContainer}
+        isOpen={open}
+        icon={{ name: "edit", color: "black" }}
+        openIcon={{ name: "close", color: "black" }}
+        onOpen={() => setOpen(!open)}
+        onClose={() => setOpen(!open)}
+      >
+        <SpeedDial.Action
+          titleStyle={{ fontFamily: "GenosR" }}
+          color="#D3D3D3"
+          icon={{ name: "map", color: "black" }}
+          title="Dashboard"
+          onPress={() => showCard(props)}
+        />
+        <SpeedDial.Action
+          color="#D3D3D3"
+          icon={{ name: "dashboard", color: "black" }}
+          title={
+            props.mapTypeProps.mapType === "standard" ? "Satellit" : "Karta"
+          }
+          onPress={() => changeMapType(props)}
+        />
+        <SpeedDial.Action
+          color="#D3D3D3"
+          icon={{ name: "settings", color: "black" }}
+          title="Heatmap Colors"
+          // onPress={() => alert('-- Color Picker --')}
+          onPress={() => setColorPickerVisible(true)}
+        />
+      </SpeedDial>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	buttonContainer: {
-		flex: 1,
-		flexDirection: 'row',
-		position: 'absolute',
-		bottom: 38,
-		paddingHorizontal: 10,
-	},
-	speedContainer: {
-		marginBottom: 23,
-	},
-})
-export default SpeedOptions
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 38,
+    paddingHorizontal: 10,
+  },
+  speedContainer: {
+    marginBottom: 23,
+  },
+});
+export default SpeedOptions;
