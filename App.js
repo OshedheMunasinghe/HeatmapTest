9cc1fd18aeb126e1164f86bdfb8f4f86fac062b9
Osh <gusmunos@student.gu.se>
2021-11-24 12:52:43 +0100

    Imported Genos-Light.ttf.

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SettingScreen from "./src/screens/Settings/SettingScreen";
 import MapScreen from "./src/screens/Map/MapScreen";
-import { useFonts } from 'expo-font';
+import {useFonts} from 'expo-font';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
     const [loaded] = useFonts({
-        GenosB: require('./assets/Fonts/Genos-Bold.ttf'),
         GenosR: require('./assets/Fonts/Genos-Regular.ttf'),
+        GenosB: require('./assets/Fonts/Genos-Bold.ttf'),
+        GenosL: require('./assets/Fonts/Genos-Light.ttf'),
     });
 
     if (!loaded) {
